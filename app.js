5c568f008446cda58a428f15c4eed9d058ff049e
SnowSurfer <drinkaziu@gmail.com>
2018-08-23 00:24:14 -0400

    update projects

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/projects", function(req, res) {
 
 
 
-app.listen(process.env.PORT, process.env.IP, function() {
-   console.log("Server is Listening!!!"); 
+app.listen(3000, function() {
+   console.log("Server is Listening!!!");
 });
